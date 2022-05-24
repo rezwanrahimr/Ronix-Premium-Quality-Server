399afaa714115d6bd9ee6161db07939612a0984a
rezwanrahimr <rezwanrahim69@gmail.com>
2022-05-24 14:15:05 +0600

    revire

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb');
+const res = require('express/lib/response');
 
 app.use(cors());
 app.use(express.json());
@@ -20,14 +22,71 @@ async function run(){
     try{
         await client.connect();
         const toolsCollection = client.db("Ronix").collection("tools");
+        const reviewsCollection = client.db("Ronix").collection("review");
 
-        // get tools.
+        // get all tools items.
         app.get("/tools",async(req,res)=>{
             const quarry = {};
             const cursor = toolsCollection.find(quarry);
             const tools = await cursor.toArray();
             res.send(tools);
         })
+
+        // Avilabe Quantity decrease.
+        app.post("/available/:id",async(req,res) => {
+            if(Number(req.body.availableQuantity < 0)){
+                res.send({status: 0, message: 'Oops! Stock out'});
+                return;
+            }
+
+            const id = req.params.id;
+            const quarry = {_id: ObjectId(id)};
+            const result = await toolsCollection.updateOne(quarry,{
+                $set: { availableQuantity: req.body.availableQuantity}
+            });
+            if(result.modifiedCount > 0){
+                res.send({ status: 1, message: "Order place successfully"});
+            }
+            else{
+                res.send({status: 0, message: "order place Faild"})
+            }
+            
+        });
+
+        // increase Quantity.
+        app.post("/increase/:id",async(req,res) => {
+            if(Number(req.body.availableQuantity < 0)){
+                res.send({status: 0, message: "Oops! Restock Faild"});
+                return;
+            }
+            const {availableQuantity,stock} = req.body;
+            const id = req.params.id;
+            const quarry = {_id: ObjectId(id)};
+            const result = await toolsCollection.updateOne(quarry,{
+                $set: {availableQuantity: Number(availableQuantity) + Number(stock)},
+            });
+            if(result.modifiedCount > 0){
+                res.send({status: 1, message: "restock successfully"});
+            }
+            else{
+                res.send({status: 0,message:"restock faild"});
+            }
+        });
+
+        // Insert Reviews.
+        app.post("/review",(req,res) =>{
+            reviewsCollection.insertOne(req.body, (err)=>{
+                if(err){
+                    res.send(err);
+                }
+                else{
+                    res.send({
+                        status: 1,
+                        message: "successfully insert one",
+                    })
+                }
+            })
+        });
 
     }
     finally{
